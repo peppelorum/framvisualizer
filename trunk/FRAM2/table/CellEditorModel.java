127cd4b3de7fab5532d5ba75916fb1b7cfbf87ea
sorno <sorno@7636a5a2-e33d-0410-a7fb-f560dcb15827>
2007-02-26 09:04:33 +0000

    new class, stores a list of CellEditors

---
+++ trunk/FRAM2/table/CellEditorModel.java
@@ -0,0 +1,35 @@
+package table;
+
+import javax.swing.table.*;
+import java.util.*;
+public class CellEditorModel
+{
+	private ArrayList<CellEditor> data;
+	
+	public CellEditorModel()
+	{
+		data = new ArrayList<CellEditor>();
+	}
+	public void addEditor(int row, int col, TableCellEditor e )
+	{
+		data.add(new CellEditor(row,col, e));
+	}
+	public void removeEditor(int row, int col)
+	{
+		for(int i=0;i<data.size();i++){
+			if(data.get(i).getCol() == col && data.get(i).getRow() == row){
+				data.remove(i);
+			}
+		}
+	}
+	public TableCellEditor getEditor(int row, int col)
+	{
+		for(int i=0;i<data.size();i++){
+			if(data.get(i).getCol() == col && data.get(i).getRow() == row){
+				return data.get(i).getEditor();
+			}
+		}
+		return null;
+	}
+}
+
