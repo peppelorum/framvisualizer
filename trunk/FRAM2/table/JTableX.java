057114693d9eebec51f8d989876543b29ceb64b0
sorno <sorno@7636a5a2-e33d-0410-a7fb-f560dcb15827>
2007-02-26 09:02:05 +0000

    Added functions for CellEditorModel

--- trunk/FRAM2/table/JTableX.java
+++ trunk/FRAM2/table/JTableX.java
@@ -1,28 +1,34 @@
 package table;
 import javax.swing.*;
 import javax.swing.table.*;
+
+import java.util.ArrayList;
 import java.util.Vector;
 
 @SuppressWarnings("serial")
 public class JTableX extends JTable {
 	protected RowEditorModel rm;
+	protected CellEditorModel cellm;
 	
 	public JTableX()
 	{
 		super();
 		rm = null;
+		cellm = null;
 	}
 	
 	public JTableX(TableModel tm)
 	{
 		super(tm);
 		rm = null;
+		cellm = null;
 	}
 	
 	public JTableX(TableModel tm, TableColumnModel cm)
 	{
 		super(tm,cm);
 		rm = null;
+		cellm = null;
 	}
 	
 	public JTableX(TableModel tm, TableColumnModel cm,
@@ -30,24 +36,28 @@ public class JTableX extends JTable {
 	{
 		super(tm,cm,sm);
 		rm = null;
+		cellm = null;
 	}
 	
 	public JTableX(int rows, int cols)
 	{
 		super(rows,cols);
 		rm = null;
+		cellm = null;
 	}
 	
 	public JTableX(final Vector rowData, final Vector columnNames)
 	{
 		super(rowData, columnNames);
 		rm = null;
+		cellm = null;
 	}
 	
 	public JTableX(final Object[][] rowData, final Object[] colNames)
 	{
 		super(rowData, colNames);
 		rm = null;
+		cellm = null;
 	}
 	
 	// new constructor
@@ -55,6 +65,24 @@ public class JTableX extends JTable {
 	{
 		super(tm,null,null);
 		this.rm = rm;
+		cellm = null;
+	}
+	public JTableX(TableModel tm, CellEditorModel cellm)
+	{
+		super(tm,null,null);
+		this.cellm = cellm;
+		this.rm = null;
+	}
+	
+	
+	public void setCellEditorModel(CellEditorModel cellm)
+	{
+		this.cellm = cellm;
+	}
+	
+	public CellEditorModel getCellEditorModel()
+	{
+		return cellm;
 	}
 	
 	public void setRowEditorModel(RowEditorModel rm)
@@ -70,10 +98,15 @@ public class JTableX extends JTable {
 	public TableCellEditor getCellEditor(int row, int col)
 	{
 		int modelColumn = convertColumnIndexToModel( col );
+		AddRowCellEditor addRow = new AddRowCellEditor(rm.getEditor(row));
+//		if(modelColumn==2){
+//			return addRow;
+//		}
 		
-		if (!(modelColumn == 1)){
+		if (!(modelColumn == 1) || (modelColumn == 2)){
 			return super.getCellEditor(row,col);
-		}	
+		}
+		
 		
 		TableCellEditor tmpEditor = null;
 		if (rm!=null)
