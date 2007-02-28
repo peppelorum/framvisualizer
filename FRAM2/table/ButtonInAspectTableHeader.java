2f24455e2e8d11e2b0ea245cf4bbad107d207f59
peppesbodega <peppesbodega@7636a5a2-e33d-0410-a7fb-f560dcb15827>
2007-02-28 08:51:51 +0000

    Added first version for buttons in the aspect header

---
+++ FRAM2/table/ButtonInAspectTableHeader.java
@@ -0,0 +1,112 @@
+package table;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.EventObject;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.event.CellEditorListener;
+import javax.swing.table.TableCellEditor;
+
+import data.Aspect;
+import data.FramNode;
+
+public class ButtonInAspectTableHeader implements ActionListener, TableCellEditor{
+	 public final Icon PLUS_ICON = new ImageIcon(getClass().getResource("plus.gif")); 
+    private TableCellEditor editor; 
+    private JButton customEditorButton = new JButton(PLUS_ICON); 
+    protected JTable table; 
+    protected int row, column; 
+    
+    public ButtonInAspectTableHeader(TableCellEditor editor){ 
+        this.editor = editor; 
+        customEditorButton.addActionListener(this); 
+ 
+        // ui-tweaking 
+        customEditorButton.setFocusable(false); 
+        customEditorButton.setFocusPainted(false); 
+        customEditorButton.setMargin(new Insets(0, 0, 0, 0)); 
+    } 
+ 
+    public Component getTableCellEditorComponent(JTable table, Object value
+                    , boolean isSelected, int row, int column){ 
+        JPanel panel = new JPanel(new BorderLayout()); 
+        panel.add(editor.getTableCellEditorComponent(table, value, isSelected, row, column)); 
+        panel.add(customEditorButton, BorderLayout.EAST); 
+        this.table = table; 
+        this.row = row; 
+        this.column = column; 
+        return panel; 
+    } 
+ 
+    public Object getCellEditorValue(){ 
+        return editor.getCellEditorValue(); 
+    } 
+ 
+    public boolean isCellEditable(EventObject anEvent){ 
+       if(editor !=null){
+    	return editor.isCellEditable(anEvent); 
+       }
+       return true;
+       } 
+ 
+    public boolean shouldSelectCell(EventObject anEvent){ 
+        return editor.shouldSelectCell(anEvent); 
+    } 
+ 
+    public boolean stopCellEditing(){ 
+        return editor.stopCellEditing(); 
+    } 
+ 
+    public void cancelCellEditing(){ 
+        editor.cancelCellEditing(); 
+    } 
+ 
+    public void addCellEditorListener(CellEditorListener l){ 
+        editor.addCellEditorListener(l); 
+    } 
+ 
+    public void removeCellEditorListener(CellEditorListener l){ 
+        editor.removeCellEditorListener(l); 
+    } 
+ 
+    public final void actionPerformed(ActionEvent e){ 
+        editor.cancelCellEditing(); 
+        selectCell(table, row, column); 
+    } 
+ 
+    protected void editCell(JTable table, int row, int column){ 
+        JTextArea textArea = new JTextArea(10, 50); 
+        Object value = table.getValueAt(row, column); 
+       
+        if(value!=null){ 
+            textArea.setText((String)value); 
+            textArea.setCaretPosition(0); 
+        } 
+        
+        FramAspectTable tablenode = (FramAspectTable)table;
+    	
+    	FramNode.NodePort conn = FramNode.NodePort.valueOf(table.getValueAt(row, 0).toString());
+    	
+    	FramNode node = tablenode.getNode();
+    	ArrayList<Aspect> aspList = node.getAttributes(conn);
+    	aspList.add(new Aspect(""));
+    	node.setAttributes(conn, aspList);
+    }
+    
+	protected void selectCell(JTable table, int row, int column) {
+		if(column > 0) {
+			editCell(table, row, column);
+		}
+	} 
+}
+
