6d748dfb4e74144485ef2eed9231f26930f7acea
sorno <sorno@7636a5a2-e33d-0410-a7fb-f560dcb15827>
2007-02-28 17:38:36 +0000

    Added GPL

--- FRAM2/table/CustomCellRenderer.java
+++ FRAM2/table/CustomCellRenderer.java
@@ -1,3 +1,27 @@
+/**
+
+ 	A visualizer for FRAM (Functional Resonance Accident Model).
+ 	This tool helps modelling the the FRAM table and visualize it.
+	Copyright (C) 2007  Peppe Bergqvist <peppe@peppesbodega.nu>, Fredrik Gustafsson <fregu808@student.liu.se>,
+	Jonas Haraldsson <haraldsson@gmail.com>, Gustav Ladén <gusla438@student.liu.se>
+	http://sourceforge.net/projects/framvisualizer/
+	
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+  
+ **/
+
 package table;
 
 import java.awt.Component;
