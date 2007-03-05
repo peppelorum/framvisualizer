8cb586506ac4a8cb18e2a42584a3fe7f4963c81d
peppesbodega <peppesbodega@7636a5a2-e33d-0410-a7fb-f560dcb15827>
2007-03-05 09:43:29 +0000

    Fixed add new row and some licensing

--- trunk/FRAM2/table/ButtonEditor.java
+++ trunk/FRAM2/table/ButtonEditor.java
@@ -1,3 +1,27 @@
+/**
+
+ 	A visualizer for FRAM (Functional Resonance Accident Model).
+ 	This tool helps modelling the the FRAM table and visualize it.
+	Copyright (C) 2007  Peppe Bergqvist <peppe@peppesbodega.nu>, Fredrik Gustafsson <fregu808@student.liu.se>,
+	Jonas Haraldsson <haraldsson@gmail.com>, Gustav Lad�n <gusla438@student.liu.se>
+	http://sourceforge.net/projects/framvisualizer/
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+ **/
+
 package table;
 
 import java.awt.Component;
@@ -102,7 +126,6 @@ public class ButtonEditor extends AbstractCellEditor implements TableCellEditor{
 		
     protected void addAspect(JTable table, int row, int column){
     	
-    	System.out.println("addAspect");
         JTextArea textArea = new JTextArea(10, 50);
         Object value = table.getValueAt(row, column);
        
@@ -117,14 +140,17 @@ public class ButtonEditor extends AbstractCellEditor implements TableCellEditor{
     	
     	FramNode node = tablenode.getNode();
     	ArrayList<Aspect> aspList = node.getAttributes(conn);
-    	aspList.add(new Aspect(""));
+    	if (aspList.size() > 1){
+    		aspList.add(row, new Aspect(""));
+    	} else {
+    		aspList.add(new Aspect(""));
+    	}
     	
     	node.setAttributes(conn, aspList);
     }
     
     protected void removeAspect(JTable table, int row, int column){
     	
-    	System.out.println("addAspect");
         JTextArea textArea = new JTextArea(10, 50);
         Object value = table.getValueAt(row, column);
        
@@ -139,9 +165,8 @@ public class ButtonEditor extends AbstractCellEditor implements TableCellEditor{
     	
     	FramNode node = tablenode.getNode();
     	ArrayList<Aspect> aspList = node.getAttributes(conn);
-    	//aspList.add(new Aspect(""));
     	if (aspList.size() > 1){
-    		aspList.remove(0);
+    		aspList.remove(row-1);
     	}
     	
     	node.setAttributes(conn, aspList);
