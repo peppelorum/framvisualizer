a909c372f372ff4f34f396ae942a45375ca891c2
peppesbodega <peppesbodega@7636a5a2-e33d-0410-a7fb-f560dcb15827>
2007-03-05 15:04:07 +0000

    Added export functionality for XML

--- FRAM2/data/Filemanager.java
+++ FRAM2/data/Filemanager.java
@@ -1,25 +1,25 @@
 /**
-
- 	A visualizer for FRAM (Functional Resonance Accident Model).
- 	This tool helps modelling the the FRAM table and visualize it.
-	Copyright (C) 2007  Peppe Bergqvist <peppe@peppesbodega.nu>, Fredrik Gustafsson <fregu808@student.liu.se>,
-	Jonas Haraldsson <haraldsson@gmail.com>, Gustav Ladén <gusla438@student.liu.se>
-	http://sourceforge.net/projects/framvisualizer/
-	
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
-	
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-	
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-  
+ 
+ A visualizer for FRAM (Functional Resonance Accident Model).
+ This tool helps modelling the the FRAM table and visualize it.
+ Copyright (C) 2007  Peppe Bergqvist <peppe@peppesbodega.nu>, Fredrik Gustafsson <fregu808@student.liu.se>,
+ Jonas Haraldsson <haraldsson@gmail.com>, Gustav Ladén <gusla438@student.liu.se>
+ http://sourceforge.net/projects/framvisualizer/
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ 
  **/
 
 package data;
@@ -30,6 +30,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintStream;
 
 /**
  * The class Filemanger handles the saving and loading to/from XML
@@ -39,8 +40,8 @@ import java.io.ObjectOutputStream;
  *
  */
 public class Filemanager {
-
-
+	
+	
 	/**
 	 * Serialize an object to file
 	 * 
@@ -68,7 +69,7 @@ public class Filemanager {
 		
 		return;
 	}
-
+	
 	/**
 	 * Load a serialized object from file
 	 * 
@@ -82,9 +83,9 @@ public class Filemanager {
 		try {
 			fis = new FileInputStream(filename);
 			ObjectInputStream ois = new ObjectInputStream(fis);
-
+			
 			obj = ois.readObject();
-
+			
 			ois.close();
 			fis.close();
 			
@@ -103,6 +104,91 @@ public class Filemanager {
 		
 		return null;
 	}
+	
+	/**
+	 * Creates an XML-file represeting a FramNodeList
+	 * 
+	 * @param nodeList List to be saved
+	 * @param filename 
+	 */
+	protected static void saveXMLFile(FramNodeList nodeList, String filename){
+		PrintStream MyOutput = null;
+		try {
+			MyOutput = new PrintStream(new FileOutputStream(filename));
+		} 
+		catch (IOException e)
+		{
+			System.out.println("Can't create file");
+		}
+		
+		
+		if (MyOutput != null) {
+			MyOutput.println("<Framlist>");
+			MyOutput.println("\t<ListName>" + nodeList.getName() + "</ListName>");
+			MyOutput.println("\t<Nodes>");
+			for(FramNode node : nodeList){
+				MyOutput.println("\t\t<FramNode>");
+				MyOutput.println("\t\t\t<Name>" + node.getName() + "</Name>");
+				MyOutput.println("\t\t\t<Comment>" + node.getComment() + "</Comment>");
+				
+				for(String[] aspect : node.getAllAspects()){
+					MyOutput.println("\t\t\t<" + aspect[0] +">");
+					MyOutput.println("\t\t\t\t<Value>" + aspect[1] + "</Value>");
+					MyOutput.println("\t\t\t\t<Comment>" + aspect[2] + "</Comment>");
+					MyOutput.println("\t\t\t</" + aspect[0] +">");
+				}
+				
+				for(String cpc : node.getCPC().getCPCTypes()){
+					if (node.getCPC().hasAttribute(cpc)) {
+						CPCAttribute a = node.getCPC().getAttribute(cpc);
+						MyOutput.println("\t\t\t<CPC>");
+						MyOutput.println("\t\t\t\t<Type>" + a.getType() + "</Type>");
+						MyOutput.println("\t\t\t\t<Value>" + a.getValue() + "</Value>");
+						MyOutput.println("\t\t\t\t<Comment>" + a.getComment() + "</Comment>");
+						MyOutput.println("\t\t\t\t<CPCForAspects>");
+						
+						boolean[] f = a.getCpcForAspects();
+						MyOutput.println("\t\t\t\t<I>" + f[0] + "</I>");
+						MyOutput.println("\t\t\t\t<O>" + f[1] + "</O>");
+						MyOutput.println("\t\t\t\t<P>" + f[2] + "</P>");
+						MyOutput.println("\t\t\t\t<R>" + f[3] + "</R>");
+						MyOutput.println("\t\t\t\t<T>" + f[4] + "</T>");
+						MyOutput.println("\t\t\t\t<C>" + f[5] + "</C>");
+						
+						MyOutput.println("\t\t\t</CPC>");
+					}
+				}
+				MyOutput.println("\t\t</FramNode>");
+			}
+			
+			MyOutput.println("\t</Nodes>");
+			MyOutput.println("<Connections>");
+			for(ConnectionInfo cInfo : nodeList.getConnections()){
+				MyOutput.println("\t<Connection>");
+				MyOutput.println("\t\t<From>");
+				MyOutput.println("\t\t<Name>" + cInfo.getFrom().getFunctionName() + "</Name>");
+				MyOutput.println("\t\t<Port>" + cInfo.getFrom().getConnectionPort() + "</Port>");
+				MyOutput.println("\t\t</From>");
+				
+				MyOutput.println("\t\t<To>");
+				MyOutput.println("\t\t<Name>" + cInfo.getFrom().getFunctionName() + "</Name>");
+				MyOutput.println("\t\t<Port>" + cInfo.getFrom().getConnectionPort() + "</Port>");
+				MyOutput.println("\t\t</To>");
+				
+				MyOutput.println("\t\t<Value>" + cInfo.getAspect() + "</Value>");
+				
+				MyOutput.println("\t</Connection>");
+				
+			}
+			
+			MyOutput.println("</Connections>");
+			MyOutput.print("</Framlist>");
+			MyOutput.close();
+		} else {
+			System.out.println("No output file written");
+		}
+	}
+	
 }
 
 
