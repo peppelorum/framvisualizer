04ea4cc423f3365a72e3d9088e6c88d5e10f2632
peppesbodega <peppesbodega@7636a5a2-e33d-0410-a7fb-f560dcb15827>
2007-03-07 10:01:18 +0000

    Add rows in aspect table works properly, but not removal..

---
+++ FRAM2/graph/NodeMenu.java
@@ -0,0 +1,119 @@
+package graph;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JComponent;
+import javax.swing.JMenuItem;
+
+public class NodeMenu extends Component {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	Visualizer vis;
+	
+	public NodeMenu(Visualizer visa, JComponent comp){
+		
+		this.vis = visa;
+				
+		JMenuItem menuItem;
+		
+		menuItem = new JMenuItem("Delete");
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				vis.deleteSelectedNode();
+				vis.repaint();
+			}
+		});
+		comp.add(menuItem);
+		
+		menuItem = new JMenuItem("Bigger");
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				vis.getSelectedNode().setSize(vis.getSelectedNode().getSize()+5);
+				vis.repaint();
+			}
+		});
+		comp.add(menuItem);
+		
+		menuItem = new JMenuItem("Smaller");
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				vis.getSelectedNode().setSize(vis.getSelectedNode().getSize()-5);
+				vis.repaint();
+			}
+		});
+		comp.add(menuItem);
+		
+		menuItem = new JMenuItem("Flag");
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				vis.getSelectedNode().setFlagged(!vis.getSelectedNode().isFlagged());
+//				updateFlagNodeButton();
+				vis.repaint();
+			}
+		});
+		comp.add(menuItem);
+		
+//		return comp;
+		
+	}
+	
+//	public JComponent create(JComponent component) {
+//		
+//		
+//		
+////		JMenuBar menuBar = new JMenuBar();
+//
+//		//...where the GUI is constructed:
+//		//Create the popup menu.
+////		popup = new JPopupMenu();
+//		
+//		JMenuItem menuItem;
+//		
+//		menuItem = new JMenuItem("Delete");
+//		menuItem.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				vis.deleteSelectedNode();
+//				vis.repaint();
+//			}
+//		});
+//		component.add(menuItem);
+//		
+//		menuItem = new JMenuItem("Bigger");
+//		menuItem.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				vis.getSelectedNode().setSize(vis.getSelectedNode().getSize()+5);
+//				vis.repaint();
+//			}
+//		});
+//		component.add(menuItem);
+//		
+//		menuItem = new JMenuItem("Smaller");
+//		menuItem.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				vis.getSelectedNode().setSize(vis.getSelectedNode().getSize()-5);
+//				vis.repaint();
+//			}
+//		});
+//		component.add(menuItem);
+//		
+//		menuItem = new JMenuItem("Flag");
+//		menuItem.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				vis.getSelectedNode().setFlagged(!vis.getSelectedNode().isFlagged());
+////				updateFlagNodeButton();
+//				vis.repaint();
+//			}
+//		});
+//		component.add(menuItem);
+//		
+//		return component;
+//	}
+
+	
+
+}
